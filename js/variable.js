074b81079ef803355d350cfcf55a5851806020ed
[Julio] <[juliocosoli@gmail.com]>
2022-09-25 10:19:23 -0300

    js

---
+++ js/variable.js
@@ -0,0 +1,22 @@
+numero=25
+console.log(numero + ' es: ' + typeof(numero))
+
+var apellido='5'
+
+console.log(apellido +' es: ' + typeof(apellido))
+
+var nulo=null
+var si=true
+
+console.log(nulo + ' es: ' + typeof(nulo))
+console.log(si + ' es: ' + typeof(si))
+
+var nada
+
+console.log(nada + ' es: ' + typeof(nada))
+apellido=Number.parseInt(apellido)
+console.log(apellido +' es: ' + typeof(apellido))
+
+var nombre =prompt('Hola, pone tu nombre para saludarte: ', '')
+
+console.log ('Hola ' + nombre + ' Como estas?')
